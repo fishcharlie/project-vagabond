95046afa9d7f13923b60fa22f55a8520d2007e39
Greg Brunk <greg.brunk@gmail.com>
2016-08-02 17:22:04 -0600

    Creates destroy route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,13 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -19,37 +18,28 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.create(user_params)
-    redirect_to "/users/#{@user.id}"
+    redirect_to user_path(@user)
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    redirect_to user_path(user)
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
   end
 
   private
