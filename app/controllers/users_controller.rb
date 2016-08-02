52aa9322b57e2dae8bb205f156bf2f7590e8b7dd
Greg Brunk <greg.brunk@gmail.com>
2016-08-02 15:36:14 -0600

    Sets up user signup and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,17 +24,8 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.create(user_params)
+    redirect_to "/users/#{@user.id}"
   end
 
   # PATCH/PUT /users/1
@@ -69,6 +60,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      user_params = params.require(:user).permit(:first_name, :last_name, :current_city, :profile_photo, :email, :password)
     end
 end
