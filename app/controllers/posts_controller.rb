e7a08ddfe52065209ac210bdadaa6cab918860c2
Greg Brunk <greg.brunk@gmail.com>
2016-08-03 16:07:38 -0600

    Adds posts user association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,23 +20,15 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-     @posts = Post.find(prams[:id])
+     @posts = Post.find(params[:id])
   end
 
   # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to user_path(current_user)
   end
 
   # PATCH/PUT /posts/1
