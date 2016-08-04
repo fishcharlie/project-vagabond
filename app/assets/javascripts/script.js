6f4099446798b3c46bba6652d5fd80e401784f63
madeleinemarie <maddy.vandenheuvel@gmail.com>
2016-08-04 13:24:54 -0600

    Fixes JQuery function so it works on all pages

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,8 +3,21 @@ $(document).ready(function() {
   var scroll_start = 0;
     var startchange = $('.start_change');
     var offset = startchange.offset();
-    $(document).scroll(function() { 
+    console.log(window);
+    $('.profile-page').on('scroll', function() { 
       scroll_start = $(this).scrollTop();
+      console.log(scroll_start);
+      console.log(offset.top);
+      if(scroll_start > (offset.top - 50)) {
+          $('.navbar').css('background-color', '#01295A');
+       } else {
+          $('.navbar').css('background-color', 'transparent');
+       }
+    });
+    $(window).on('scroll', function() { 
+      scroll_start = $(this).scrollTop();
+      console.log(scroll_start);
+      console.log(offset.top);
       if(scroll_start > (offset.top - 50)) {
           $('.navbar').css('background-color', '#01295A');
        } else {
@@ -12,3 +25,4 @@ $(document).ready(function() {
        }
     });
 });
+
