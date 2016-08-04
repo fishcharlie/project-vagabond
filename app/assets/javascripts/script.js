b4aaed4e6382fcdc354f356130308f76ee1429cf
madeleinemarie <maddy.vandenheuvel@gmail.com>
2016-08-04 11:02:12 -0600

    Creates JQuery effect where nav bar changes backgrounds when necessary

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
   var scroll_start = 0;
-    var startchange = $('#startchange');
+    var startchange = $('.start_change');
     var offset = startchange.offset();
     $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('.navbar_default').css('background-color', 'black');
+      if(scroll_start > (offset.top - 50)) {
+          $('.navbar').css('background-color', '#01295A');
        } else {
-          $('.navbar_default').css('background-color', 'transparent');
+          $('.navbar').css('background-color', 'transparent');
        }
     });
 });
